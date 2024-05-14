b86c169e0d905dfaacd2520cb51d65fac6cda5a8
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-14 14:50:53 -0400

    Add button click event and input handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // User can type task into input field and click submit
+  //User should then see the string provided appear in the DOm after submit
+
+  const handleInput = () => {
+    const input = document.getElementById("new-task-description")
+    const userInput = input.value
+    console.log(userInput)
+  }
+
+  //grab user submission into input field 
+  addEventListener("click", (event) => {
+    event.preventDefault()
+    const userInput = handleInput()
+  });
+
 });
