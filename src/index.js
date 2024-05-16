0a3a03e37d2042ea874d7524fac0175ccda21455
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-16 10:11:49 -0400

    Add prioirty dropdown and apply to tasks

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ document.addEventListener("DOMContentLoaded", () => {
   submitBtn.addEventListener("click", (event) => {
     event.preventDefault()
     const userInput = getUserInput()
+    const priority = document.getElementById("priority").value
     const tasks = document.getElementById("tasks")
     const todo = document.createElement("li")
     const deleteBtn = document.createElement("button")
     deleteBtn.addEventListener("click", handleDelete)
     deleteBtn.textContent = 'X'
     todo.textContent = `${userInput} `
+    todo.style.color = priority
     todo.appendChild(deleteBtn)
     tasks.appendChild(todo)
   })
